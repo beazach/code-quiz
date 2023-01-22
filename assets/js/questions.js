de9b98f3ce4f16aa66b497b43c4c73c0d454a423
bea zacharska <beata.zacharska@gmail.com>
2023-01-22 15:53:33 +0000

    added questions to the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,28 @@
 // Creating an object that loads questions
 let questions = [
     {
-        "title": "Common Data Types DO NOT include:",
-        "choices": ["string", "booleans", "alerts", "numbers"],
-        "answer": "alerts"
+        "title": "__________ Keyword is used to create Package in java.",
+        "choices": ["Pack", "None", "Class", "Interface"],
+        "answer": "None"
     },
     {
-        "title": "Common Data Types include:",
-        "choices": ["string", "booleans", "alerts", "numbers"],
-        "answer": "string"
+        "title": "The TreeMap class is used to implement:",
+        "choices": ["NavigableMap", "AbstractMap Class", "Map interface", "All of the above"],
+        "answer": "All of the above"
     },
     {
-        "title": "Question 3:",
-        "choices": ["string", "booleans", "alerts", "numbers"],
-        "answer": "numbers"
+        "title": "Which inheritance is achieved using interface in Java?",
+        "choices": ["Single Inheritance", "Multiple Inheritance", "Multilevel Inheritance", "None of the above"],
+        "answer": "Multiple Inheritance"
     },
     {
-        "title": "Question 4:",
-        "choices": ["string", "booleans", "alerts", "numbers"],
-        "answer": "booleans"
+        "title": "Methods of the interface must not be ___ in nature.",
+        "choices": ["Native", "Final", "Static", "All of the above"],
+        "answer": "All of the above"
+    },
+    {
+        "title": "How many classes we are able to extend in Java?",
+        "choices": ["Only one class", "Two classes", "Three classes", "Four classes"],
+        "answer": "Only one class"
     }
 ]
